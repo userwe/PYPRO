74b3f867178c88349fd5779053c4d29135399724
wangwen <wangwen5201@outlook.com>
2019-06-25 22:33:15 +0800

    111

---
+++ test123.py
@@ -0,0 +1,16 @@
+# 通例
+
+hlist =  ["哈哈","小杜","订单","方法","订单"]
+print(hlist)
+
+for i in hlist
+    print(i)
+
+bbbb = {
+    
+}
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(i,"X",j,"=",i*j,"\t",end="")
+    print("")
