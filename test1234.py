eec721732eac249dd52d95db475fafecb88295f8
wangwen <wangwen5201@outlook.com>
2019-06-26 21:29:13 +0800

    123.dm

---
+++ test1234.py
@@ -0,0 +1,12 @@
+hlist = [34,44,24,89,99,78,57,89,54,78,46,87,97,100,7,56]
+lowlist = []
+highlist = []
+for i in hlist
+    if i > 60:
+        highlist.append(i)
+    else:
+        lowlist.append(i)   
+print(hlist)
+print(lowlist)
+print(highlist)
+         
